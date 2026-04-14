4fdc1d813b502de887e7bb90daa7e7d758c83d61
lisantov <lisantov@yandex.ru>
2026-04-14 19:48:28 +0700

    Add: logger pull by env

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,15 +3,29 @@ import { AppModule } from './app.module';
 import 'dotenv/config';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { ValidationPipe } from '@nestjs/common';
-import {DevLogger, TskvLogger} from "./loggers";
+import { DevLogger, TskvLogger, JsonLogger } from "./loggers";
+
+const getLoggerFromEnvironment = () => {
+    switch(process.env.LOGGER_TYPE) {
+        case "dev":
+            return new DevLogger("LOGGER_INFO");
+        case "json":
+            return new JsonLogger();
+        case "tskv":
+            return new TskvLogger();
+        default:
+            throw new Error("Unsupported logger type");
+    }
+}
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
       bufferLogs: true,
   });
+
   app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix('api/afisha');
-  app.useLogger(new TskvLogger());
+  app.useLogger(getLoggerFromEnvironment());
   app.enableCors();
   await app.listen(3000);
 }
